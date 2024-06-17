33b793c71354fb897d0cc749be0ac70b6d20d333
2001win <81064313+2001win@users.noreply.github.com>
2024-06-17 15:08:11 +0700

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+const NEXT_PUBLIC_POLYGON_MUMBAI_RPC = "https://rpc-amoy.polygon.technology/";
+const NEXT_PUBLIC_PRIVATE_KEY =
+  "7a41d97ae57e82fd5807780eb7cd42bfef80061e81b7c586eff8873a1efdf9cf";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+module.exports = {
+  solidity: "0.8.9",
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {},
+    polygon_amoy: {
+      url: NEXT_PUBLIC_POLYGON_MUMBAI_RPC,
+      accounts: [`0x${NEXT_PUBLIC_PRIVATE_KEY}`],
+    },
+  },
+};
